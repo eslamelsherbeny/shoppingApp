57feafabc4c1e798f86d6f99e94204a0fd337568
eslamm <eslamelshereny16@gmail.com>
2025-12-17 23:48:08 +0200

    geed

--- services/addressService.js
+++ services/addressService.js
@@ -1,104 +1,55 @@
 const asyncHandler = require('express-async-handler')
-
 const User = require('../models/userModel')
 
-// @desc    Add address to user addresses list
-// @route   POST /api/v1/addresses
+// @desc    Add product to wishlist
+// @route   POST /api/v1/wishlist
 // @access  Protected/User
-exports.addAddress = asyncHandler(async (req, res, next) => {
-  // $addToSet => add address object to user addresses array if address not exist
+exports.addProductToWishlist = asyncHandler(async (req, res, next) => {
+  // $addToSet => add productId to wishlist array if productId not exist
   const user = await User.findByIdAndUpdate(
     req.user._id,
     {
-      $addToSet: { addresses: req.body },
+      $addToSet: { wishlist: req.body.productId },
     },
     { new: true }
-  )
+  ).populate('wishlist') // ✅ التعديل هنا: أضفنا populate
 
   res.status(200).json({
     status: 200,
-    message: 'Address added successfully.',
-    data: user.addresses,
+    message: 'Product added successfully to your wishlist.',
+    data: user.wishlist,
   })
 })
 
-// ✅ إضافة دالة التعديل
-// @desc    Update specific address from user addresses list
-// @route   PUT /api/v1/addresses/:addressId
+// @desc    Remove product from wishlist
+// @route   DELETE /api/v1/wishlist/:productId
 // @access  Protected/User
-exports.updateAddress = asyncHandler(async (req, res, next) => {
-  const { addressId } = req.params
-  const { alias, details, phone, city, postalCode } = req.body
-
-  // Find user
-  const user = await User.findById(req.user._id)
-
-  if (!user) {
-    return res.status(404).json({
-      status: 404,
-      message: 'User not found',
-    })
-  }
-
-  // Find address index
-  const addressIndex = user.addresses.findIndex(
-    (addr) => addr._id.toString() === addressId
-  )
-
-  if (addressIndex === -1) {
-    return res.status(404).json({
-      status: 404,
-      message: 'Address not found',
-    })
-  }
-
-  // Update address fields
-  if (alias !== undefined) user.addresses[addressIndex].alias = alias
-  if (details !== undefined) user.addresses[addressIndex].details = details
-  if (phone !== undefined) user.addresses[addressIndex].phone = phone
-  if (city !== undefined) user.addresses[addressIndex].city = city
-  if (postalCode !== undefined)
-    user.addresses[addressIndex].postalCode = postalCode
-
-  // Save user
-  await user.save()
-
-  res.status(200).json({
-    status: 200,
-    message: 'Address updated successfully.',
-    data: user.addresses[addressIndex],
-  })
-})
-
-// @desc    Remove address from user addresses list
-// @route   DELETE /api/v1/addresses/:addressId
-// @access  Protected/User
-exports.removeAddress = asyncHandler(async (req, res, next) => {
-  // $pull => remove address object from user addresses array if addressId exist
+exports.removeProductFromWishlist = asyncHandler(async (req, res, next) => {
+  // $pull => remove productId from wishlist array if productId exist
   const user = await User.findByIdAndUpdate(
     req.user._id,
     {
-      $pull: { addresses: { _id: req.params.addressId } },
+      $pull: { wishlist: req.params.productId },
     },
     { new: true }
-  )
+  ).populate('wishlist') // ✅ التعديل هنا: أضفنا populate
 
   res.status(200).json({
     status: 200,
-    message: 'Address removed successfully.',
-    data: user.addresses,
+    message: 'Product removed successfully from your wishlist.',
+    data: user.wishlist,
   })
 })
 
-// @desc    Get logged user addresses list
-// @route   GET /api/v1/addresses
+// @desc    Get logged user wishlist
+// @route   GET /api/v1/wishlist
 // @access  Protected/User
-exports.getLoggedUserAddresses = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user._id).populate('addresses')
+exports.getLoggedUserWishlist = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id).populate('wishlist')
 
   res.status(200).json({
     status: 200,
-    results: user.addresses.length,
-    data: user.addresses,
+    results: user.wishlist.length,
+    data: user.wishlist,
   })
 })
